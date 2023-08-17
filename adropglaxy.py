a2669260d7915470f03eacfdca2578b92829a006
knownblackhat <knownblackhat@offsec>
2023-08-17 07:59:50 -0400

    fix: dropglxy bypasser

--- adropglaxy.py
+++ adropglaxy.py
@@ -1,70 +1,71 @@
-import re
 import asyncio
+import logging
+from selectolax.parser import HTMLParser
+import re
 import sys
-from typing import AsyncGenerator, List
-from bs4 import BeautifulSoup
+from typing import Set
+
 import httpx
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(levelname)s: %(message)s",
+    level=logging.INFO,
+    handlers=[logging.NullHandler()],
+)
+logger = logging.getLogger(__name__)
 
 
 class DropGalaxy:
     def __init__(self, client: httpx.AsyncClient) -> None:
-        self.base_url = "https://dropgalaxy.co/"
+        self.base_url = "https://dropgalaxy.co/drive/"
         self.client = client
 
     async def _get_token(self, id: str) -> str:
         token = f"[hostname=dropgalaxy.co][id={id}]"
         msg = [ord(x) for x in token]
-        msg = str(msg).replace('[', '').replace(']', '')
-        msg = msg.replace('2', '004').replace('3', '005').replace('7', '007')
-        msg = re.sub(r',0,0,0', '', msg)
+        msg = str(msg).replace("[", "").replace("]", "")
+        msg = msg.replace("2", "004").replace("3", "005").replace("7", "007")
+        msg = re.sub(r",0,0,0", "", msg)
         headers = {
-            'authority': 'tmp.isavetube.com',
-            'accept': '*/*',
-            'accept-language': 'en-US,en;q=0.8',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'origin': 'https://dropgalaxy.co',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+            "authority": "tmp.isavetube.com",
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.8",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "origin": "https://dropgalaxy.co",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
         }
-        data = {
-                'rand': '',
-                'msg': msg
-                }
+        data = {"rand": "", "msg": msg}
 
-        resp = await self.client.post('https://tmp.isavetube.com/gettoken.php',
-                                      data=data,
-                                      headers=headers)
+        resp = await self.client.post(
+            "https://tmp.isavetube.com/gettoken.php", data=data, headers=headers
+        )
         return resp.text
 
-    async def _get_link(self, id: str) -> str:
+    async def _get_link(self, id: str) -> str | None:
         headers = {
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
         }
-        data = {
-            'op': 'download2',
-            'id': id,
-            'xd': await self._get_token(id)
-        }
+        data = {"op": "download2", "id": id, "xd": await self._get_token(id)}
 
-        resp = await self.client.post(self.base_url + id,
-                                      headers=headers,
-                                      data=data)
-        soup = BeautifulSoup(resp.content, 'html.parser')
-        form_element = soup.find('form', id='dllink')
-        if form_element:
-            zip_url = form_element.get('action')  # type: ignore
-            return zip_url  # type: ignore
-
+        resp = await self.client.post(self.base_url + id, headers=headers, data=data)
+        html = HTMLParser(resp.content)
+        try:
+            zip_url = html.css_first('#dllink').attributes.get('action')
+        except AttributeError:
+            logger.critical(f"Unable to get zip url {data=!r}")
+            return None
         else:
-            raise ValueError()
+            return zip_url
 
     def _link_id(self, url: str) -> str:
-        return url.split('/')[-1]
+        return url.split("/")[-1]
 
-    async def __call__(self, url_array: List[str]) -> List[str]:
-        ids = (self._link_id(url) for url in url_array)
-        tasks = (self._get_link(id) for id in ids)
+    async def __call__(self, url_array: Set[str]) -> Set[str]:
+        ids = {self._link_id(url) for url in url_array}
+        tasks = {self._get_link(id) for id in ids}
         urls = await asyncio.gather(*tasks)
-        return urls
+        return set(urls)
 
 
 async def main() -> None:
@@ -75,10 +76,10 @@ async def main() -> None:
         exit()
     async with httpx.AsyncClient() as client:
         resolver = DropGalaxy(client)
-        links = await resolver(urls)
+        links = await resolver(set(urls))
         for link in links:
             print(link)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
